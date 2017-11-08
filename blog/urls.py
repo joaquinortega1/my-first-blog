af7d42695d7c29c9f846126e1173204eb75cb807
joaquinortega <joakoortega99@hotmail.com>
2017-11-08 12:48:47 -0300

    Agregue views para crear o editar blog post adentro del sitio

--- blog/urls.py
+++ blog/urls.py
@@ -1,6 +1,10 @@
 from django.conf.urls import include, url
 from . import views
 
+
+
 urlpatterns = [
-    url(r'^$', views.post_list),
+    url(r'^$', views.post_detail),
+    url(r'^post/(?P<pk>[0-9]+)/$', views.post_detail, name='post_detail'),
 ]
+
